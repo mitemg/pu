14e57ad791f5252d0895829777e400d999ebaad0
mitemg <misiqmg@abv.bg>
2018-06-01 18:24:43 +0300

    addin js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
 function openSite(){
 	window.open("https://uni-plovdiv.bg/");
 }
+
+function openWiki(){
+	window.open("https://bg.wikipedia.org/wiki/%D0%9F%D0%BB%D0%BE%D0%B2%D0%B4%D0%B8%D0%B2%D1%81%D0%BA%D0%B8_%D1%83%D0%BD%D0%B8%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D1%82%D0%B5%D1%82");
+}
